18d2ab5c4418c20a3d97394acf7f5989e5c1c5ee
saebyeok chu <cuu2252@gmail.com>
2024-08-30 17:25:15 +0900

    edit design_list

--- wc/models.py
+++ wc/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Design(models.Model) :
-    userId = models.IntegerField()
-    list = models.TextField()
+    userid = models.IntegerField()
+    mediaid = models.IntegerField()
+    shared = models.BooleanField()
+    createdat = models.DateTimeField()
 
     
