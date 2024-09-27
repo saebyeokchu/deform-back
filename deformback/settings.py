8c337600d401194f5a326a950c7ba0ee2b0ae8c3
saebyeok chu <cuu2252@gmail.com>
2024-09-27 14:43:42 +0900

    [deploy] update aws deploy yml

--- deformback/settings.py
+++ deformback/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'deform', 
         'USER': 'postgres',
-        'PASSWORD': 'deformeditoradmin',
+        'PASSWORD': '1234',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
