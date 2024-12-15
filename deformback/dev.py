104e14b3e9d3f3c363c06b9c90eab4fe39c22f5a
saebyeok chu <cuu2252@gmail.com>
2024-12-15 19:59:24 +0900

    add color and env file

--- deformback/dev.py
+++ deformback/dev.py
@@ -1,13 +1,17 @@
+from decouple import config
 from .base import *
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "59.23.104.33"]
+
+DATABASE_USER = config('DEV_DB_USER')
+DATABASE_PASSWORD = config('DEV_DB_PASSWORD')
 
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'editor',
-            'USER': 'postgres',
-            'PASSWORD': '1234', #dgepw
+            'USER': DATABASE_USER,
+            'PASSWORD': DATABASE_PASSWORD, #dgepw
             'HOST': '127.0.0.1',
             'PORT': '5432',
             }
--- deformback/docker.py
+++ deformback/docker.py
@@ -1,13 +1,17 @@
 from .base import *
+from decouple import config
 
 ALLOWED_HOSTS = ["localhost", "59.23.104.33"]
+
+DATABASE_USER = config('DOCKER_DB_USER')
+DATABASE_PASSWORD = config('DOCKER_DB_PASSWORD')
 
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'editor',
-            'USER': 'postgres',
-            'PASSWORD': 'dgepw', #deformeditoradmin
+            'USER': DATABASE_USER,
+            'PASSWORD': DATABASE_PASSWORD, #deformeditoradmin
             'HOST': 'host.docker.internal',
             'PORT': '5432',
             }
